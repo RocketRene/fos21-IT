3fc8fc2cdd6e42c21c021fefcb0efdd5a25d7048
René <renekkuhn@posteo.de>
2023-01-19 09:35:05 +0100

    add atomic int

--- Person.java
+++ Person.java
@@ -1,14 +1,30 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Person {
     private String name;
     private Datum geburtsdatum;
+    private static final AtomicInteger count = new AtomicInteger(0);
+    private final int ID;
+
+
 
     public Person(String name, Datum geburtsdatum) {
         this.name = name;
         this.geburtsdatum = geburtsdatum;
+        ID = count.incrementAndGet();
     }
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", geburtsdatum=" + geburtsdatum +
+                ", ID=" + ID +
+                '}';
     }
 
     public void setName(String name) {
@@ -24,5 +40,7 @@ public class Person {
         Person person = new Person("Max Mustermann", geburtsdatum);
         System.out.println("Name: " + person.getName());
         System.out.println("Geburtsdatum: " + person.getGeburtsdatum());
-    }
-}
+        System.out.println(person.toString());
+        Person person2 = new Person("Tester",geburtsdatum);
+        System.out.println(person2);
+}}
